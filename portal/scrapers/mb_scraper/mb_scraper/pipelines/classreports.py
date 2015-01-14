3c706f702dae1d3bca2ca2468ebe3b2f26045b23
Adam Morris <amorris@mistermorris.com>
2015-01-14 21:58:32 +0800

    refactored database pipelining

--- portal/scrapers/mb_scraper/mb_scraper/pipelines/classreports.py
+++ portal/scrapers/mb_scraper/mb_scraper/pipelines/classreports.py
@@ -72,15 +72,16 @@ class PYPClassReportsPipline(PostgresPipeline):
     def allow_this_spider(self, spider):
         return spider.name.startswith('PYPClassReports')
 
-    def make_primary_report(term_id, course_id, student_id, teacher_id=None, homeroom_comment=""):
+    def make_primary_report(self, term_id, course_id, student_id, teacher_id=None, homeroom_comment=""):
         PrimaryReport = self.database.table_string_to_class('Primary_Report')
 
         exists = None
         with DBSession() as session:
             try:
                 exists = session.query(PrimaryReport).filter_by(term_id=term_id, course_id=course_id, student_id=student_id).one()
-                if exists:
-                    exists.homeroom_comment = item['homeroom_comment']
+                if exists and homeroom_comment:
+                    exists.homeroom_comment = homeroom_comment
+                return exists
             except NoResultFound:
                 primary_report = PrimaryReport(
                         course_id = course_id,
@@ -90,9 +91,57 @@ class PYPClassReportsPipline(PostgresPipeline):
                         student_id = student_id
                     )
                 session.add(primary_report)
-        if exists:
-            return exists
-        return session.query(PrimaryReport).filter_by(term_id=term_id, course_id=course_id, student_id=student_id).one()
+                return primary_report
+
+    def make_primary_report_section(self, term_id, subject_id, course_id, student_id, comment="", name =""):
+        exists = None
+        PrimaryReportSection = self.database.table_string_to_class('Primary_Report_Section')
+        Teacher = self.database.table_string_to_class('Advisor')
+        TeacherAssignments = self.database.table_string_to_class('Primary_Teacher_Assignments')
+
+        with DBSession() as session:
+
+            # Set up the teachers
+            teachers = []
+            teacher_assignments = session.query(TeacherAssignments).filter_by(subject_id=subject_id, class_id=course_id).all()
+            for teacher in teacher_assignments:
+                teachers.append( session.query(Teacher).filter_by(id=teacher.teacher_id).one() )
+
+            primary_report = self.make_primary_report(term_id, course_id, student_id)
+
+            try:
+                exists = session.query(PrimaryReportSection).filter_by(primary_report_id=primary_report.id, subject_id=subject_id).one()
+                if exists:
+                    if comment: 
+                        exists.comment = comment
+                    if name:
+                        exists.name = name
+
+                # Sync the teachers, removing those that are gone, adding if any needed to be added
+                for teacher in exists.teachers:
+                    if not teacher in teachers:
+                        exists.teachers.remove(teacher)
+
+                for teacher in teachers:
+                    if not teacher in exists.teachers:
+                        exists.teachers.append(teacher)
+
+                return exists
+
+            except NoResultFound:
+                # Shouldn't there be a teacher in here somewhere?
+                primary_report_section = PrimaryReportSection(
+                        primary_report_id = primary_report.id,
+                        subject_id = subject_id,
+                        comment = comment,
+                        name = name,
+                    )
+                session.add(primary_report_section)
+
+                for teacher in teachers:
+                    primary_report_section.teachers.append( teacher )
+
+                return primary_report_section
 
 
     def database_add(self, key, item):
@@ -107,7 +156,8 @@ class PYPClassReportsPipline(PostgresPipeline):
             term_id = int(item.get('term_id'))
             course_id = int(item.get('course_id'))
             student_id = int(item.get('student_id'))
-            make_primary_report(term_id, course_id, student_id, None, "")
+
+            self.make_primary_report(term_id, course_id, student_id, None, "")
 
         elif item.__class__ is PrimaryReportSectionItem:
             term_id = int(item.get('term_id'))
@@ -115,48 +165,9 @@ class PYPClassReportsPipline(PostgresPipeline):
             student_id = int(item.get('student_id'))
             subject_id = int(item.get('subject_id'))
 
-            # Just make sure it's already set up
-            with DBSession() as session:
-
-                # Set up the teachers
-                teachers = []
-                teacher_assignments = session.query(TeacherAssignments).filter_by(subject_id=subject_id, class_id=course_id).all()
-                for teacher in teacher_assignments:
-                    teachers.append( session.query(Teacher).filter_by(id=teacher.teacher_id).one() )
-
-                try:
-                    primary_report = make_primary_report(term_id, course_id, student_id)
-                except NoResultFound:
-                    print("NO PRIMARY REPORT??")
-                    from IPython import embed
-                    embed()
-                try:
-                    exists = session.query(PrimaryReportSection).filter_by(primary_report_id=primary_report.id, subject_id=subject_id).one()
-                    exists.comment = item['comment']
-                    exists.name = item.get('subject_name', '')
-
-                    # Sync the teachers, removing those that are gone, adding if any needed to be added
-                    for teacher in exists.teachers:
-                        if not teacher in teachers:
-                            exists.teachers.remove(teacher)
-
-                    for teacher in teachers:
-                        if not teacher in exists.teachers:
-                            exists.teachers.append(teacher)
-
-                except NoResultFound:
-                    # Shouldn't there be a teacher in here somewhere?
-                    primary_report_section = PrimaryReportSection(
-                            primary_report_id = primary_report.id,
-                            subject_id = subject_id,
-                            comment = item['comment'],
-                            name = item.get('subject_name', ''),
-                        )
-                    session.add(primary_report_section)
-
-                    for teacher in teachers:
-                        primary_report_section.teachers.append( teacher )
-                    
+            self.make_primary_report_section(term_id, subject_id, course_id, student_id,
+                comment=item.get('comment'), name=item.get('subject_name')
+                )                    
 
         elif issubclass(item.__class__, PrimaryReportSupplementItem):
             # First get the id of the primary report, for convenience
@@ -168,13 +179,9 @@ class PYPClassReportsPipline(PostgresPipeline):
 
             with DBSession() as session:
                 # Primary report should already be in the database
-                try:
-                    primary_report = session.query(PrimaryReport).filter_by(term_id=term_id, course_id=course_id, student_id=student_id).one()
-                    primary_report_section = session.query(PrimaryReportSection).filter_by(primary_report_id=primary_report.id, subject_id=subject_id).one()
-                except NoResultFound:
-                    print("No Primary / PrimarySection?")
-                    from IPython import embed
-                    embed()
+                primary_report = self.make_primary_report(term_id, course_id, student_id)
+                primary_report_section = self.make_primary_report_section(term_id, subject_id, course_id, student_id)
+
                 primary_report_id = primary_report.id
                 primary_report_section_id = primary_report_section.id
 
