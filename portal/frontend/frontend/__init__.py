591f88493df96272129409a544371f8b213a49ea
Adam Morris <amorris@mistermorris.com>
2015-02-22 20:00:16 +0800

    include pyramid_beaker

--- portal/frontend/frontend/__init__.py
+++ portal/frontend/frontend/__init__.py
@@ -18,7 +18,11 @@ def main(global_config, **settings):
     DBSession.configure(bind=engine)
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
+    
+    # include third-party packages here
     config.include('pyramid_chameleon')
+    config.include('pyramid_beaker')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('auditlog', '/auditlog')
