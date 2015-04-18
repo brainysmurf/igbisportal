f83d75bcb6b043ed5739715607af67e77bf33a16
Adam Morris <amorris@mistermorris.com>
2015-04-18 10:34:39 +0800

    forgot js files

---
+++ portal/frontend/frontend/static/js/reports_hub/init.js
@@ -0,0 +1,27 @@
+
+(function () {
+
+	$('#userEnter').autocomplete({
+		delay: 0,
+		autoFocus: true,
+		minLength: 3,
+		source: $('#userEnter').data('autocomplete-source'),
+
+		// focus: function (event, ui) {
+		// 	$('#userEnter').val(ui.item.value);
+		// },
+
+		select: function (event, ui) {
+			console.log(ui.item);
+			$('#studentID').val(ui.item.id);
+			//$('#userSubmit').removeAttr('disabled');
+			var url = "students/" + $('#studentID').val() + "/pyp_report"
+			window.location.href = url;
+		},
+
+		// search: function (event, ui) {
+		// 	$('#userSubmit').attr('disabled', 'disabled');
+		// }
+
+	});
+})();
