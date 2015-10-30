e018a19de845af6de986c62a148e88ccdb5064b5
brainysmurf <amorris@mistermorris.com>
2015-10-30 11:12:45 +0800

    refactor to make openapply_importer

--- cli/openapply_importer.py
+++ cli/openapply_importer.py
@@ -21,8 +21,17 @@ class OA_Medical_Importer:
 	def __init__(self, path):
 		self.path = path
 
-	def read_in(self):
+	@classmethod
+	def from_file(cls, path):
+		me = cls(path)
+		return me
 
+	@classmethod
+	def from_api(cls):
+		me = cls(None)
+		return me
+
+	def read_in_from_file(self):
 		with open(self.path, 'rb') as _file:
 			reader = csv.reader(_file, dialect='excel')
 			headers = reader.next()
@@ -55,6 +64,46 @@ class OA_Medical_Importer:
 
 				updater(obj)
 
+	def read_in_from_api(self):
+		with open(self.path, 'rb') as _file:
+			reader = csv.reader(_file, dialect='excel')
+			headers = reader.next()
+
+			translate_headers = lambda x: x.replace(' ', '_').replace('/', '_').replace('-', '')
+			headers = [translate_headers(x) for x in headers]
+
+			for row in reader:
+				# Get the student info first and then go through the items
+				student_id = row[0]
+				if not student_id:
+					click.echo("Student info with no student_id? {}".format(":".join(row)))
+					continue
+				with DBSession() as session:
+					try:
+						student = session.query(Students).filter_by(student_id=student_id).one()
+					except NoResultFound:
+						student = None
+				if not student:
+					click.echo('Student not found {}'.format(student_id))
+					continue
+
+				obj = MedInfo()
+				obj.id = student.id
+
+				for i in range(1, len(headers)):
+					header = headers[i]
+					item = row[i]
+					setattr(obj, header, item)
+
+				updater(obj)
+
+
+	def read_in(self):
+
+		if self.path:
+			self.read_in_from_file()
+		else:
+			self.read_in_from_api()
 
 if __name__ == "__main__":
 	read_in()
