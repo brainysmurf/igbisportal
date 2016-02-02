4bccbfe0e6bf9f7681aec9d58c07daabaa037f40
Adam Morris <amorris@mistermorris.com>
2016-02-02 21:08:36 +0800

    update

--- portal/db/UpdaterHelper.py
+++ portal/db/UpdaterHelper.py
@@ -129,15 +129,15 @@ class updater_helper:
 				session.add(obj)
 				row = None
 
-			verbose = True
+			verbose = False
 
 			if row:
 				# Get list of columns that the object possesses and are available in the database
 				column_names = [c.name for c in row.__table__.columns if c.name != 'id']
 				column_names = [c for c in column_names if hasattr(obj, c)]
 
-				if hasattr(row, 'student_id') and row.student_id == '20300008':
-					from IPython import embed;embed();exit()
+				# if hasattr(row, 'student_id') and row.student_id == '20300008':
+				# 	from IPython import embed;embed();exit()
 
 				for column in column_names:
 					if verbose:
