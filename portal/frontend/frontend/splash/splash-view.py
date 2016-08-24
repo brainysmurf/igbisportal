5ce5cca03927aec901f3ef522e806fde47216077
Adam Morris <amorris@mistermorris.com>
2016-08-24 11:26:27 +0800

    admin adjustments

--- portal/frontend/frontend/splash/splash-view.py
+++ portal/frontend/frontend/splash/splash-view.py
@@ -149,6 +149,8 @@ administration_buttons = [
             menu_item(icon="folder", display="SS Camps", url="https://docs.google.com/spreadsheets/d/1l4rF_xnyy3hjKOGftBaOfnACq-PIz2hgT2uIR_Jyd2g/edit#gid=798482198"),
         ]
         }),
+    button(name="Gmail", externalid=-9, size="", color="green", url="https://gmail.com", icon="envelope", id="", 
+        context_menu=None),
     button(name="Communications", externalid=-9, size="large", color="aqua", url="https://sites.google.com/a/igbis.edu.my/communications/", icon="comments", id="", context_menu={
     'items': [
         menu_item(icon="plus-circle", display="Add a Daily Notice", url="https://docs.google.com/a/igbis.edu.my/forms/d/1ni0lu4mzVFzHW8PxWyap8iUEYFjOjEi_z_ZjEDJaS-s/viewform"),
@@ -158,7 +160,6 @@ administration_buttons = [
     ]}),
     bookings_button,
     button(name="Calendar", externalid=-9, size="", color="peach", url="https://sites.google.com/a/igbis.edu.my/igbis-calendar/", icon="calendar", id="", context_menu=None),
-    emergency_button,
 ]
 
 @view_config(route_name='updateButtons', renderer='json')
