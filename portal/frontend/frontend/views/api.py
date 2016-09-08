9637e97d1092fe195495d810f3fe8bddc26a5a01
brainysmurf <amorris@mistermorris.com>
2016-09-08 15:57:33 +0800

    better api_students, includes ordery_by

--- portal/frontend/frontend/views/api.py
+++ portal/frontend/frontend/views/api.py
@@ -58,38 +58,22 @@ def api_students(request):
     if secret != gns.config.api.secret:
         return dict(message="IGBIS api is not for public consumption.", data=[])
     derived_attr = json_body.get('derived_attr')
-    filter = json_body.get('filter')
+    filter_by = json_body.get('filter_by')
+    if filter_by:
+        try:
+            filter_by = getattr(Student, filter_by)
+        except AttributeError:
+            filter_by = None
     awesome_table_filters = json_body.get('awesome_table_filters') or {}
     google_sheets_format = json_body.get('google_sheets_format') or True
     column_map = json_body.get('column_map') or {}
     human_columns = json_body.get('human_columns') or True
-    passed_columns = json_body.get('columns') or False
-
-    if derived_attr:
-        field_name = derived_attr.get('field')
-        string_pattern = derived_attr.get('string')
-
-        # Now use the awesome chameleon to render it as a templating language!
-        # TODO: validate the pattern, ensuring that ${things} are in __dict__?
-        template = PageTemplate(string_pattern)
-
-        if field_name and string_pattern:
-            # Define a new field!
-            setattr(Students, field_name, hybrid_property(lambda self_: template.render(**self_.columns_hybrids_dict)))
-
-    if derived_attr:
-        columns = [field_name, 'student_id', 'email']
-    else:
-        columns = []
-
-    if not passed_columns:
-        # Add in the extra columns
+    columns = json_body.get('columns') or []
+    order_by_column = json_body.get('order_by') or None
+    every_column = json_body.get('every_column') or False
+    if every_column:
         column_attrs = Students.columns_and_hybrids()
         columns.extend([c for c in column_attrs if c not in columns])
-    else:
-        # Just put in the ones that are requested
-        # TODO: Validate, return a useful error
-        columns.extend(passed_columns)
 
     google_sheets_format = True #'Google-Apps-Script' in request.agent or json_body.get('google_sheets_format') or 
     data = []
@@ -111,18 +95,16 @@ def api_students(request):
                     Students.status == 'enrolled'
                 ))
 
-        if filter == 'filterSecondary':
-            query = query.filter(Students.grade >= 6)
+        if order_by_column:
+            query = query.order_by(getattr(Students, order_by_column))
 
-        elif filter == 'filterElementary':
-            query = query.filter(Students.grade < 6)
-
-        if columns[0] == 'grade_first_nickname_last_studentid':
-            query = query.order_by(Students.grade, Students.first_name)
-        elif columns[0] == 'grade_last_first_nickname_studentid':
-            query = query.order_by(Students.grade, Students.last_name)
         else:
-            query = query.order_by(getattr(Students, columns[0]))
+            # We want to automatically sort by the first column: 
+            #    exception: if the first column startswith "grade", in which case we want to sort by integer grade rather than string grade
+            if columns and columns[0].startswith('grade_'):
+                query = query.order_by(Students.grade, Students.first_name)
+            else:
+                query = query.order_by(getattr(Students, columns[0]))
 
         data = query.all()
 
