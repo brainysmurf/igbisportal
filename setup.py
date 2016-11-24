ae00a2e1f833f7881e2e26264725dc88c11c3c01
Adam Morris <amorris@mistermorris.com>
2016-11-24 21:16:31 +0800

    added dependency

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     'pysftp',
     'oauth2client',
     'pysmb',
-    'aiohttp', 'cchardet', 'aiodns',
+    'aiohttp', 'cchardet', 'aiodns', 'aiofiles'
 ],
     # also requires?
     #'scrapyd', 'lxml', 'w3lib', 'cssselect', 'pysqlite'
