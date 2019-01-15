c9b1fa2d640d275d98e9fdf3997964677c37426a
Adam Morris <amorris@mistermorris.com>
2019-01-15 09:18:15 +0800

    added students

--- portal/frontend/frontend/splash/splash-view.py
+++ portal/frontend/frontend/splash/splash-view.py
@@ -267,8 +267,11 @@ def splash(request):
         counselor_button_students,
         button(name="Houses", externalid=-9, size="", color="orange", url="https://sites.google.com/igbis.edu.my/igbhouses/home", icon="home", id="", context_menu=None),
         button(name="DP Programme", externalid=-9, size="", color="", url="https://sites.google.com/igbis.edu.my/diplomaprogramme", icon="graduation-cap", id="", context_menu=None),
-        secondary_camps_button,
-        button(name="Student-led Event Proposal", externalid=-9, size="", id="", color="cyan", url="https://docs.google.com/document/d/1PbyENnoX_sCImyLippTdrk364b3NoKgbxKf01jCJjjU/copy", icon="calendar", context_menu=None)
+        button(name="Student-led Event Proposal", externalid=-9, size="", id="", color="cyan", url="https://docs.google.com/document/d/1PbyENnoX_sCImyLippTdrk364b3NoKgbxKf01jCJjjU/copy", icon="calendar", context_menu=None),
+        button(name="EOSMun19", externalid=-9, size="", id="", color="yellow", url="https://eosmun19.wixsite.com/2019-eosmun", icon="renren", context_menu=None),
+        button(name="Abstract Reality by Marvin", externalid=-9, size="", id="", color="grey", url="https://www.kongregate.com/games/marvinch/abstract-reality", icon="gamepad", context_menu=None),
+        button(name="Feedback on Abstract Reality", externalid=-9, size="", id="", color="blue", url="https://docs.google.com/forms/d/e/1FAIpQLSfa5v4EzzJzhxnvQGKqHx1EeZ0K3bYdWHOrfYjIkwfPZrI7cg/viewform", icon="list-ul", context_menu=None),
+
     ])
 
     with DBSession() as session:
